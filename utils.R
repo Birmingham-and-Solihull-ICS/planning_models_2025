6647bce4efaa989b6ae93b2541750020b21c9930
Chris Mainey <c.mainey1@nhs.net>
2025-12-04 08:57:54 +0000

    Remove temporary sim function, add Rcpp compile for sim functions

--- utils.R
+++ utils.R
@@ -1,3 +1,5 @@
+library(Rcpp)
+
 calc_relief_capacity2 <-
     function(
         demand,
@@ -25,114 +27,114 @@ calc_relief_capacity2 <-
     }
 
 
+#
+#
+# wl_simulator <- function(
+#         start_date = NULL,
+#         end_date = NULL,
+#         demand = 10,
+#         capacity = 11,
+#         waiting_list = NULL
+# ) {
+#     # Validate inputs
+#     NHSRwaitinglist:::check_date(start_date, end_date, .allow_null = TRUE)
+#     NHSRwaitinglist:::check_class(demand, capacity, .expected_class = "numeric")
+#     if (!is.null(waiting_list)) NHSRwaitinglist:::check_wl(waiting_list)
+#
+#     # Set default dates
+#     if (is.null(start_date)) start_date <- Sys.Date()
+#     if (is.null(end_date)) end_date <- start_date + 31
+#
+#     start_date <- as.Date(start_date)
+#     end_date <- as.Date(end_date)
+#     number_of_days <- as.integer(end_date - start_date)
+#
+#     # Compute demand and capacity
+#     total_demand <- demand * number_of_days / 7
+#     daily_capacity <- capacity / 7
+#
+#     # Realized demand and referral dates
+#     realized_demand <- stats::rpois(1, total_demand)
+#     days_seq <- seq(start_date, end_date, by = "day")
+#     referral <- sort(sample(days_seq, realized_demand, replace = TRUE))
+#
+#     # Initialize removal column
+#     removal <- as.Date(rep(NA_integer_, length(referral)), origin = "1970-01-01")
+#
+#     # # Handle withdrawal
+#     # if (is.na(withdrawal_prob)) withdrawal_prob <- 0.1
+#     # withdrawal <- referral + rgeom(length(referral), prob = withdrawal_prob) + 1
+#     # withdrawal[withdrawal > end_date] <- NA
+#
+#     # Build simulated waiting list
+#     wl_simulated <- data.frame(
+#         Referral = referral,
+#         Removal = removal#,
+#         # Withdrawal = withdrawal
+#     )
+#
+#     # Merge with existing waiting list if provided
+#     if (!is.null(waiting_list)) {
+#         wl_simulated <- wl_join(waiting_list, wl_simulated)
+#     }
+#
+#     # Create operating schedule if capacity > 0
+#     if (daily_capacity > 0) {
+#         schedule <- sim_schedule(number_of_days, start_date, daily_capacity)
+#         wl_simulated <- wl_schedule(wl_simulated, schedule)
+#     }
+#
+#     return(wl_simulated)
+# }
+#
+#
+#
+# wl_simulator_fast <- function(
+#         start_date,
+#         end_date,
+#         demand = 10,
+#         capacity = 11,
+#         waiting_list = NULL
+# ) {
+#     # Assume start_date and end_date are Date objects (convert once outside)
+#     number_of_days <- as.integer(end_date - start_date)
+#     if (number_of_days <= 0) return(waiting_list)
+#
+#     # Compute demand and capacity
+#     total_demand <- demand * number_of_days / 7
+#     daily_capacity <- capacity / 7
+#
+#     # Realized demand and referral dates
+#     realized_demand <- stats::rpois(1, total_demand)
+#     if (realized_demand > 0) {
+#         # Faster sampling: sample positions, then map to days
+#         days_seq <- seq.int(0, number_of_days)  # offsets
+#         referral_offsets <- sample.int(length(days_seq), realized_demand, replace = TRUE)
+#         referral <- start_date + referral_offsets
+#     } else {
+#         referral <- as.Date(integer(0), origin = "1970-01-01")
+#     }
+#
+#     # Removal column preallocated
+#     removal <- rep(NA, length(referral))
+#
+#     # Build simulated waiting list
+#     wl_simulated <- data.frame(Referral = referral, Removal = removal)
+#
+#     # Merge with existing waiting list if provided
+#     if (!is.null(waiting_list)) {
+#         wl_simulated <- wl_join(waiting_list, wl_simulated)  # optimize wl_join separately
+#     }
+#
+#     # Apply schedule if capacity > 0
+#     if (daily_capacity > 0 && nrow(wl_simulated) > 0) {
+#         schedule <- sim_schedule(number_of_days, start_date, daily_capacity)
+#         wl_simulated <- wl_schedule(wl_simulated, schedule)  # optimize wl_schedule separately
+#     }
+#
+#     wl_simulated
+# }
 
 
-wl_simulator <- function(
-        start_date = NULL,
-        end_date = NULL,
-        demand = 10,
-        capacity = 11,
-        waiting_list = NULL
-) {
-    # Validate inputs
-    NHSRwaitinglist:::check_date(start_date, end_date, .allow_null = TRUE)
-    NHSRwaitinglist:::check_class(demand, capacity, .expected_class = "numeric")
-    if (!is.null(waiting_list)) NHSRwaitinglist:::check_wl(waiting_list)
-
-    # Set default dates
-    if (is.null(start_date)) start_date <- Sys.Date()
-    if (is.null(end_date)) end_date <- start_date + 31
-
-    start_date <- as.Date(start_date)
-    end_date <- as.Date(end_date)
-    number_of_days <- as.integer(end_date - start_date)
-
-    # Compute demand and capacity
-    total_demand <- demand * number_of_days / 7
-    daily_capacity <- capacity / 7
-
-    # Realized demand and referral dates
-    realized_demand <- stats::rpois(1, total_demand)
-    days_seq <- seq(start_date, end_date, by = "day")
-    referral <- sort(sample(days_seq, realized_demand, replace = TRUE))
-
-    # Initialize removal column
-    removal <- as.Date(rep(NA_integer_, length(referral)), origin = "1970-01-01")
-
-    # # Handle withdrawal
-    # if (is.na(withdrawal_prob)) withdrawal_prob <- 0.1
-    # withdrawal <- referral + rgeom(length(referral), prob = withdrawal_prob) + 1
-    # withdrawal[withdrawal > end_date] <- NA
-
-    # Build simulated waiting list
-    wl_simulated <- data.frame(
-        Referral = referral,
-        Removal = removal#,
-        # Withdrawal = withdrawal
-    )
-
-    # Merge with existing waiting list if provided
-    if (!is.null(waiting_list)) {
-        wl_simulated <- wl_join(waiting_list, wl_simulated)
-    }
-
-    # Create operating schedule if capacity > 0
-    if (daily_capacity > 0) {
-        schedule <- sim_schedule(number_of_days, start_date, daily_capacity)
-        wl_simulated <- wl_schedule(wl_simulated, schedule)
-    }
-
-    return(wl_simulated)
-}
-
-
-
-wl_simulator_fast <- function(
-        start_date,
-        end_date,
-        demand = 10,
-        capacity = 11,
-        waiting_list = NULL
-) {
-    # Assume start_date and end_date are Date objects (convert once outside)
-    number_of_days <- as.integer(end_date - start_date)
-    if (number_of_days <= 0) return(waiting_list)
-
-    # Compute demand and capacity
-    total_demand <- demand * number_of_days / 7
-    daily_capacity <- capacity / 7
-
-    # Realized demand and referral dates
-    realized_demand <- stats::rpois(1, total_demand)
-    if (realized_demand > 0) {
-        # Faster sampling: sample positions, then map to days
-        days_seq <- seq.int(0, number_of_days)  # offsets
-        referral_offsets <- sample.int(length(days_seq), realized_demand, replace = TRUE)
-        referral <- start_date + referral_offsets
-    } else {
-        referral <- as.Date(integer(0), origin = "1970-01-01")
-    }
-
-    # Removal column preallocated
-    removal <- rep(NA, length(referral))
-
-    # Build simulated waiting list
-    wl_simulated <- data.frame(Referral = referral, Removal = removal)
-
-    # Merge with existing waiting list if provided
-    if (!is.null(waiting_list)) {
-        wl_simulated <- wl_join(waiting_list, wl_simulated)  # optimize wl_join separately
-    }
-
-    # Apply schedule if capacity > 0
-    if (daily_capacity > 0 && nrow(wl_simulated) > 0) {
-        schedule <- sim_schedule(number_of_days, start_date, daily_capacity)
-        wl_simulated <- wl_schedule(wl_simulated, schedule)  # optimize wl_schedule separately
-    }
-
-    wl_simulated
-}
-
-
-
+# Build Rcpp function
 Rcpp::sourceCpp("wl_simulator.cpp")
